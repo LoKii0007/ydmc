196b6fd0360207b4e3d9628998bfac89aceb3e95
LoKii0007 <107889670+LoKii0007@users.noreply.github.com>
2024-01-10 17:41:09 +0530

    Update api.jsx

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import toast from "react-hot-toast"
 
-const VERCELURL = "https://ydmc-backend.vercel.app"
+const URL = "https://ydmc-backend.vercel.app"
 // const URL = "http://localhost:8000"
 
 
