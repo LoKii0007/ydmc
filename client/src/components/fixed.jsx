e6a1599d829dbc2076b7464cde3188bdf9990523
Lokii0007 <lokeshyadav1098@gmail.com>
2024-01-25 18:43:20 +0530

    fixed

--- client/src/components/fixed.jsx
+++ client/src/components/fixed.jsx
@@ -6,7 +6,7 @@ export default function Fixed() {
         <>
             <div className="fixed position-fixed m-4">
                 <a href="https://wa.me/9560545070" target="blank" rel="noopener noreferrer">
-                    <i class="fa-brands fa-whatsapp p-1 m-2"/>
+                <img className='whatsapp-icon' src="/whatsapp-icon.png" alt="" />
                 </a>
             </div>
         </>
